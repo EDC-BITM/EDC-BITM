5422a4ae3ff3da0e901cb57d5f48eab93baf388b
coastaldemigod <shivanshagrawal2411@gmail.com>
2022-02-05 23:48:17 +0530

    startup directory created

--- data/startup-data.js
+++ data/startup-data.js
@@ -1,1 +1,456 @@
 // startup-data
+
+const data = [
+    {
+        "name": "ANAND SINGH",
+        "graduated": 2016,
+        "course": "B.E.(ECE)",
+        "linkedin": "linkedin.com/in/anand-singh-1b12851a7",
+        "photo": "",
+        "startup": "AIcharya",
+        "startup-nature": "SaaS",
+        "startup-website": "https://www.aicharya.com",
+        "start-location": "Mumbai"
+    },
+    {
+        "name": "Abhishek Kumar",
+        "graduated": 2008,
+        "course": "MCA",
+        "linkedin": "linkedin.com/in/askabhi",
+        "photo": "",
+        "startup": "Virinchi Software Pvt. Ltd. , docOPD Technologies Pvt Ltd",
+        "startup-nature": "Software, Healthcare",
+        "startup-website": "https://virinchisoftware.com",
+        "start-location": "New Delhi"
+    },
+    {
+        "name": "Abhishek Sinha",
+        "graduated": 1999,
+        "course": "B.E in ECE",
+        "linkedin": "linkedin.com/in/abhishek-sinha-8366721b4",
+        "photo": "",
+        "startup": "Eko.in",
+        "startup-nature": "Fintech",
+        "startup-website": "https://eko.in/",
+        "start-location": "Gurugram"
+    },
+    {
+        "name": "Akshay Jalan",
+        "graduated": 2014,
+        "course": "B.E in ECE",
+        "linkedin": "linkedin.com/in/akshayjalan",
+        "photo": "",
+        "startup": "Brainsmith",
+        "startup-nature": "Consumer Goods",
+        "startup-website": "http://www.brainsmith.in",
+        "start-location": "Mumbai"
+    },
+    {
+        "name": "Amit Chaudhary",
+        "graduated": 2009,
+        "course": "BE in IT",
+        "linkedin": "linkedin.com/in/4daxis",
+        "photo": "",
+        "startup": "Lenskart",
+        "startup-nature": "Tech Retail",
+        "startup-website": "http://www.lenskart.com",
+        "start-location": "New Delhi"
+    },
+    {
+        "name": "Anil Kumar Peri",
+        "graduated": 2019,
+        "course": "BSc in Mathematics and Computing",
+        "linkedin": "linkedin.com/in/anilkumarperi",
+        "photo": "",
+        "startup": "SwiftSkill",
+        "startup-nature": "Software",
+        "startup-website": "https://www.swiftskill.gg",
+        "start-location": "London"
+    },
+    {
+        "name": "Anjali",
+        "graduated": 2009,
+        "course": "BE in CSE",
+        "linkedin": "linkedin.com/in/jsranjali",
+        "photo": "",
+        "startup": "In-Biot Private Limted",
+        "startup-nature": "Education Technology",
+        "startup-website": "https://inbiot.co.in/",
+        "start-location": "Bangalore"
+    },
+    {
+        "name": "Ankit Jain",
+        "graduated": 2009,
+        "course": "BE CSE",
+        "linkedin": "linkedin.com/in/jainankit01",
+        "photo": "",
+        "startup": "MyOperator",
+        "startup-nature": "B2B SaaS",
+        "startup-website": "https://www.myoperator.com",
+        "start-location": "Delhi"
+    },
+    {
+        "name": "Arjun Goyal",
+        "graduated": 2011,
+        "course": "BE in Civil",
+        "linkedin": "linkedin.com/in/arjungoyal",
+        "photo": "",
+        "startup": "Grassroots Economics, Environment & Tech Advancement Foundation",
+        "startup-nature": "Research Foundation",
+        "startup-website": "https://gf-india.com/",
+        "start-location": "New Delhi"
+    },
+    {
+        "name": "Arun Kumar Gautam",
+        "graduated": 2012,
+        "course": "BHMCT",
+        "linkedin": "linkedin.com/in/arun-kumar-gautam-3a71141",
+        "photo": "",
+        "startup": "Gautam and Gautam Group",
+        "startup-nature": "Tour and Travel Services, Graphic Design, Social Marketing, Daily Need Delivery, Music Editing",
+        "startup-website": "http://gautamandgautamgroup.org/",
+        "start-location": "Chennai"
+    },
+    {
+        "name": "Ayush Agarwal",
+        "graduated": 1990,
+        "course": "EEE",
+        "linkedin": "linkedin.com/in/ayushkumaragarwal",
+        "photo": "",
+        "startup": "Tanaashi Technologies Pvt Ltd",
+        "startup-nature": "Software",
+        "startup-website": "https://tanaashi.com",
+        "start-location": "Noida"
+    },
+    {
+        "name": "Chandra bhushan sharma",
+        "graduated": 2017,
+        "course": "CSE",
+        "linkedin": "linkedin.com/in/chandra-bhushan-sharma-0b3701a7",
+        "photo": "",
+        "startup": "Meathut India pvt ltd., ursoftlab pvt ltd. , adived ecommerce",
+        "startup-nature": "Software service/ecommerce/msme",
+        "startup-website": "http://www.meathut.in",
+        "start-location": "Bhore , Gopalganj, bihar"
+    },
+    {
+        "name": "Karan bajaj",
+        "graduated": 2000,
+        "course": "BE in Mechanical Engineering",
+        "linkedin": "linkedin.com/in/karan-bajaj-8806191",
+        "photo": "",
+        "startup": "whitehat jr.",
+        "startup-nature": "Ed-tech",
+        "startup-website": "https://www.whitehatjr.com/",
+        "start-location": "Mumbai"
+    },
+    {
+        "name": "Karthik Reddy",
+        "graduated": 2016,
+        "course": "BE in ECE",
+        "linkedin": "linkedin.com/in/karthikvummadi",
+        "photo": "",
+        "startup": "iB Hubs",
+        "startup-nature": "Advanced Tech",
+        "startup-website": "https://ibhubs.co/",
+        "start-location": "Hyderabad"
+    },
+    {
+        "name": "Kumar Tanmay",
+        "graduated": 2007,
+        "course": "BE in Biotechnology",
+        "linkedin": "linkedin.com/in/ktanmay",
+        "photo": "",
+        "startup": "Inkredo",
+        "startup-nature": "B2B SaaS",
+        "startup-website": "https://www.inkredo.in",
+        "start-location": "New Delhi"
+    },
+    {
+        "name": "Lakshmi Dheshik Reddy N",
+        "graduated": 2016,
+        "course": "B.E in ECE",
+        "linkedin": "linkedin.com/in/dheshikreddyn",
+        "photo": "",
+        "startup": "Minikroft",
+        "startup-nature": "Agri-Tech",
+        "startup-website": "https://www.minikroft.com",
+        "start-location": "Hyderabad"
+    },
+    {
+        "name": "Nikhil Mantha",
+        "graduated": 2012,
+        "course": "B.E in ECE",
+        "linkedin": "linkedin.com/in/nikhil-mantha-1b14241b",
+        "photo": "",
+        "startup": "Piggy",
+        "startup-nature": "Fintech",
+        "startup-website": "https://www.piggy.co.in/wealth/",
+        "start-location": "Bengaluru"
+    },
+    {
+        "name": "Nitin Jain",
+        "graduated": 2011,
+        "course": "Bachelors in Animation & Multimedia, Masters in Animation & Design",
+        "linkedin": "linkedin.com/in/indianimator",
+        "photo": "",
+        "startup": "Indibni Private Limited",
+        "startup-nature": "Product Design and Ecommerce",
+        "startup-website": "https://www.indibni.com",
+        "start-location": "Jaipur"
+    },
+    {
+        "name": "Nitin Kamal",
+        "graduated": 2015,
+        "course": "B.E. in C.S.E.",
+        "linkedin": "linkedin.com/in/nitin-kamal-a2841a80",
+        "photo": "",
+        "startup": "The Data Monk",
+        "startup-nature": "E-Learning",
+        "startup-website": "http://thedatamonk.com/",
+        "start-location": "Gurgaon, Haryana"
+    },
+    {
+        "name": "Pawan Kumar Gupta",
+        "graduated": 2009,
+        "course": "UG",
+        "linkedin": "linkedin.com/in/pguptasloan",
+        "photo": "",
+        "startup": "Betterhalf.ai",
+        "startup-nature": "Matchmaking (relationship)",
+        "startup-website": "https://betterhalf.ai/",
+        "start-location": "Bangalore"
+    },
+    {
+        "name": "Pawan kumar rai",
+        "graduated": 2014,
+        "course": "BE in Production Engineering",
+        "linkedin": "linkedin.com/in/pawan-kumar-rai-2a437a61",
+        "photo": "",
+        "startup": "finshots & ditto insurance",
+        "startup-nature": "Financial Newsletter,Insurance",
+        "startup-website": "https://finshots.in/",
+        "start-location": "Bengaluru"
+    },
+    {
+        "name": "Piyush Priyam",
+        "graduated": 2008,
+        "course": "BE in Mechanical engineering",
+        "linkedin": "linkedin.com/in/piyush-priyam",
+        "photo": "",
+        "startup": "PEPO",
+        "startup-nature": "B2B SaaS",
+        "startup-website": "http://www.pepo.one/",
+        "start-location": "Bangalore"
+    },
+    {
+        "name": "Pratik Kumar",
+        "graduated": 2008,
+        "course": "B.E. in Mechanical Engineering",
+        "linkedin": "linkedin.com/in/pratik-kumar-92453a157",
+        "photo": "",
+        "startup": "Awishcar.com",
+        "startup-nature": "Software",
+        "startup-website": "http://www.awishcar.com",
+        "start-location": "Jamshedpur"
+    },
+    {
+        "name": "Pratyush Agarwal",
+        "graduated": 2016,
+        "course": "UG, B.E in IT",
+        "linkedin": "linkedin.com/in/pratsy",
+        "photo": "",
+        "startup": "CodeAsylums",
+        "startup-nature": "Education, Technology Products",
+        "startup-website": "https://www.codeasylums.com",
+        "start-location": "Bangalore"
+    },
+    {
+        "name": "Rahul Anand",
+        "graduated": 2009,
+        "course": "BE/CSE",
+        "linkedin": "linkedin.com/in/rahul-anand-a834691a",
+        "photo": "",
+        "startup": "Pioneer Bfactory Consultancy & Services Private Limited",
+        "startup-nature": "Ed-tech",
+        "startup-website": "https://www.bfactory.in/",
+        "start-location": "Ranchi"
+    },
+    {
+        "name": "Sanjit Chatterjee",
+        "graduated": 1987,
+        "course": "B Pharmacy",
+        "linkedin": "linkedin.com/in/sanjit-chatterjee-2b7557b",
+        "photo": "",
+        "startup": "Blueneck Consulting",
+        "startup-nature": "Career Consulting",
+        "startup-website": "https://www.blueneck.net",
+        "start-location": "Mumbai"
+    },
+    {
+        "name": "Shaleen Mathur",
+        "graduated": 2009,
+        "course": "B.E in ECE",
+        "linkedin": "linkedin.com/in/mathurshaleen",
+        "photo": "",
+        "startup": "Pink Sarahna Foundation",
+        "startup-nature": "Blockchain Platform & Social Currency",
+        "startup-website": "https://www.sarahna.io",
+        "start-location": "New Delhi"
+    },
+    {
+        "name": "Shaleen Mathur",
+        "graduated": 2009,
+        "course": "B.E in ECE",
+        "linkedin": "linkedin.com/in/mathurshaleen",
+        "photo": "",
+        "startup": "Pink Sarahna Foundation",
+        "startup-nature": "Blockchain Platform & Social Currency",
+        "startup-website": "https://www.sarahna.io",
+        "start-location": "New Delhi"
+    },
+    {
+        "name": "Shantanu Singh",
+        "graduated": 2000,
+        "course": "B.E. in ECE",
+        "linkedin": "linkedin.com/in/shantanu-singh-476b541a2",
+        "photo": "",
+        "startup": "Daily Salary",
+        "startup-nature": "Technology (HR Tech / Fintech)",
+        "startup-website": "https://dailysalary.in",
+        "start-location": "Gurgaon"
+    },
+    {
+        "name": "Shaurya Shahi",
+        "graduated": 2015,
+        "course": "BE IT",
+        "linkedin": "linkedin.com/in/shaurya-shahi",
+        "photo": "",
+        "startup": "HeapTree",
+        "startup-nature": "Software",
+        "startup-website": "https://link.medium.com/af0TXWwAz8",
+        "start-location": "Bangalore"
+    },
+    {
+        "name": "Shivam Raj",
+        "graduated": 2021,
+        "course": "B.E in CSE",
+        "linkedin": "linkedin.com/in/sh1v4mr4j",
+        "photo": "",
+        "startup": "Doorcha",
+        "startup-nature": "Consumer goods",
+        "startup-website": "https://doorcha.com",
+        "start-location": "Ranchi"
+    },
+    {
+        "name": "Sougata Basu",
+        "graduated": 2006,
+        "course": "EEE",
+        "linkedin": "linkedin.com/in/sougatab",
+        "photo": "",
+        "startup": "CashRich",
+        "startup-nature": "Fintech",
+        "startup-website": "https://www.cashrich.com",
+        "start-location": "Mumbai"
+    },
+    {
+        "name": "Sougata Basu",
+        "graduated": 2006,
+        "course": "EEE",
+        "linkedin": "linkedin.com/in/sougatab",
+        "photo": "",
+        "startup": "CashRich",
+        "startup-nature": "Fintech",
+        "startup-website": "https://www.cashrich.com",
+        "start-location": "Mumbai"
+    },
+    {
+        "name": "Vishal anand",
+        "graduated": 2018,
+        "course": "B.E. ECE",
+        "linkedin": "linkedin.com/in/vishal-anand-51bb49110",
+        "photo": "",
+        "startup": "OLSO (Gearide rental pvt ltd)",
+        "startup-nature": "Rental Mrketplace",
+        "startup-website": "https://olsorental.com",
+        "start-location": "Ranchi"
+    }
+]
+
+// Some parts of these datas are missing.
+const missingData=[
+    {
+        "name": "Micky Midha",
+        "graduated": 2009,
+        "course": "BE in CSE (UG)",
+        "linkedin": "",
+        "photo": "",
+        "startup": "Midha Education Private Limited",
+        "startup-nature": "e-Learning",
+        "startup-website": "https://midhafin.com/",
+        "start-location": "Ranchi"
+    },
+    {
+        "name": "Nitesh Kumar",
+        "graduated": 2013,
+        "course": "B.E in Chemical and Polymer Engineering",
+        "linkedin": "",
+        "photo": "",
+        "startup": "Vaxia.com ( a B2B eCommerce company for industrial MRO products)",
+        "startup-nature": "eCommerce",
+        "startup-website": "https://www.vaxia.com",
+        "start-location": "Bangalore"
+    },
+    {
+        "name": "Srikar",
+        "graduated": 2017,
+        "course": "B.E in Production Engineering",
+        "linkedin": "",
+        "photo": "",
+        "startup": "iB Hubs",
+        "startup-nature": "Technology",
+        "startup-website": "https://ibhubs.co/",
+        "start-location": "Gurgaon, Delhi"
+    },
+    {
+        "name": "VISHAL KUMAR",
+        "graduated": 2009,
+        "course": "B.E in ECE",
+        "linkedin": "",
+        "photo": "",
+        "startup": "RANCHI BAZAAR",
+        "startup-nature": "CONSUMER GOODS",
+        "startup-website": "https://ranchi-bazaar-vivek-vastralaya.business.site/",
+        "start-location": "SATYANARAYAN MARKET, UPPER BAZAR, RANCHI"
+    }
+]
+
+
+const container = document.getElementById("listOfStartups");
+
+// Not needed now, but present here, for future reference, when more startups are added.
+/*data.sort((a,b) => {
+    if (a.name < b.name)
+        return -1;
+    return 1;
+});*/
+
+for (let i = 0; i < data.length; i++) {
+    container.innerHTML += (`
+      <div class="member-container">
+                    <div class="member">
+                        <div class="details">
+                            <h4 class="name">${data[i]['name']}</h4>
+                            <h6 class="position">${data[i]['course']},${data[i]['graduated']}</h6>
+                            <a class="startup-link" href="${data[i]['startup-website']}">
+                                <h5 class="position-link">${data[i]['startup']},${data[i]['start-location']}</h5>
+                            </a>
+                            <h5 class="position">${data[i]['startup-nature']}</h5>
+                        </div>
+                        <div class="social">
+                            <a href="https://www.${data[i]['linkedin']}"><i class="fab fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+      `);
+}
