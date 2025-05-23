d1ff5e003dada77feab150477d95dfcf1ab085bf
Salil Lakra <salillakra.dev@gmail.com>
2025-05-23 16:42:48 +0530

    feat(Sponser): refactor component for improved layout and readability, enhance AOS attributes and remove linting errors

--- src/components/Sponser/Sponser.jsx
+++ src/components/Sponser/Sponser.jsx
@@ -1,138 +1,220 @@
-import React from 'react'
-import img1 from '../Sponser/1113718.jpg'
-import img2 from '../Sponser/radio.png'
-import img3 from '../Sponser/hiralal.jpg'
+import img1 from "../Sponser/1113718.jpg";
+import img2 from "../Sponser/radio.png";
+import img3 from "../Sponser/hiralal.jpg";
 
 function Sponser() {
   return (
-
-    <div data-aos="fade-up"  data-aos-anchor-placement="center-bottom"data-aos-duration="3000">
-      <div className='mt-12'>
-        <h2 className='font-bold ml-24 spon1 text-[25px]' ><span className=' bg-[#FED853] pt-2 pb-2 pl-3'>Sp</span>onsorship</h2>
-        <h1 className='text-2xl font-bold ml-24 mt-5 spon1'>Our Past Sponsors</h1>
-        <div className='flex overflow-hidden mt-10'>
-          <div className='slide flex animate-infinte-slide shrink-0 whitespace-nowrap gap-12 mx-2'>
-          <div className='h-40 w-40 flex justify-center items-center'>
-            <a href="https://logowik.com/nissan-new-logo-vector-31776.html"><img src="https://logowik.com/content/uploads/images/nissan-new2693.logowik.com.webp"/></a>
+    <div
+      data-aos="fade-up"
+      data-aos-anchor-placement="center-bottom"
+      data-aos-duration="3000"
+    >
+      <div className="mt-12">
+        <h2 className="font-bold ml-8 sm:ml-24 spon1 text-[25px]">
+          <span className=" bg-[#FED853] pt-2 pb-2 pl-3">Sp</span>onsorship
+        </h2>
+        <h1 className="text-2xl font-bold ml-8 sm:ml-24 mt-5 spon1">
+          Our Past Sponsors
+        </h1>
+        <div className="flex overflow-hidden mt-10">
+          <div className="slide flex animate-infinte-slide shrink-0 whitespace-nowrap gap-12 mx-2">
+            <div className="h-40 w-40 flex justify-center items-center">
+              <a href="https://logowik.com/nissan-new-logo-vector-31776.html">
+                <img src="https://logowik.com/content/uploads/images/nissan-new2693.logowik.com.webp" />
+              </a>
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <a href="https://logowik.com/puma-vector-logo-1-4481.html"><img src="https://logowik.com/content/uploads/images/puma.jpg"/></a>
+            <div className="h-40 w-40 flex justify-center items-center">
+              <a href="https://logowik.com/puma-vector-logo-1-4481.html">
+                <img src="https://logowik.com/content/uploads/images/puma.jpg" />
+              </a>
             </div>
-          <div className='h-40 w-40 flex justify-center items-center'>
-          <a href="https://logowik.com/nikon-vector-logo-2425.html"><img src="https://logowik.com/content/uploads/images/741_nikon.jpg"/></a>
+            <div className="h-40 w-40 flex justify-center items-center">
+              <a href="https://logowik.com/nikon-vector-logo-2425.html">
+                <img src="https://logowik.com/content/uploads/images/741_nikon.jpg" />
+              </a>
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://img.icons8.com/?size=100&id=AbQBhN9v62Ob&format=png&color=000000"/>
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img src="https://img.icons8.com/?size=100&id=AbQBhN9v62Ob&format=png&color=000000" />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://logowik.com/content/uploads/images/mahindra-suv1293.jpg"/>
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img src="https://logowik.com/content/uploads/images/mahindra-suv1293.jpg" />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src={img1}/>
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img src={img1} />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src={img2}/>
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img src={img2} />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src={img3}/>
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img src={img3} />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <a href="https://logowik.com/rapido-bike-taxi-vector-logo-16011.html"><img src="https://logowik.com/content/uploads/images/rapido-bike-taxi8263.jpg"/></a>
+            <div className="h-40 w-40 flex justify-center items-center">
+              <a href="https://logowik.com/rapido-bike-taxi-vector-logo-16011.html">
+                <img src="https://logowik.com/content/uploads/images/rapido-bike-taxi8263.jpg" />
+              </a>
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp9dqdnVXkrnk-DizHUu-a-SuiF3XWqhULbA&s" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp9dqdnVXkrnk-DizHUu-a-SuiF3XWqhULbA&s"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img  src="https://gengigih.demopreview.link/assets/logo-partners/progate-logo.png" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://gengigih.demopreview.link/assets/logo-partners/progate-logo.png"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://logowik.com/content/uploads/images/pizza-hut-new6371.jpg" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://logowik.com/content/uploads/images/pizza-hut-new6371.jpg"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpgHrtdptFtKYOLL4elhWf4pxOfHjNUSxr7Q&s" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpgHrtdptFtKYOLL4elhWf4pxOfHjNUSxr7Q&s"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://coding-blocks.github.io/assets/images/cb/cblogo.png" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://coding-blocks.github.io/assets/images/cb/cblogo.png"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcikSC2e1l_KFqdwrzZj8AwTpVE06gp7okJQ&s" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcikSC2e1l_KFqdwrzZj8AwTpVE06gp7okJQ&s"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://www.teqfocus.com/wp-content/uploads/2022/08/logo99-1.jpg" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://www.teqfocus.com/wp-content/uploads/2022/08/logo99-1.jpg"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://cdn.dribbble.com/users/873027/screenshots/3996894/dribbble_001_2x.png" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://cdn.dribbble.com/users/873027/screenshots/3996894/dribbble_001_2x.png"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTrEfVcpVCq0lWB0CmMYyM8KBHlxJy1n9RVQ&s" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTrEfVcpVCq0lWB0CmMYyM8KBHlxJy1n9RVQ&s"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZShoVBhCZoflB-I35i_BsVH9Lexeanbbq2Q&s" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZShoVBhCZoflB-I35i_BsVH9Lexeanbbq2Q&s"
+                alt=""
+              />
             </div>
           </div>
-          <div className=' flex animate-infinte-slide shrink-0 whitespace-nowrap gap-12 mx-2'>
-          <div className='h-40 w-40 flex justify-center items-center'>
-            <a href="https://logowik.com/nissan-new-logo-vector-31776.html"><img src="https://logowik.com/content/uploads/images/nissan-new2693.logowik.com.webp"/></a>
+          <div className=" flex animate-infinte-slide shrink-0 whitespace-nowrap gap-12 mx-2">
+            <div className="h-40 w-40 flex justify-center items-center">
+              <a href="https://logowik.com/nissan-new-logo-vector-31776.html">
+                <img src="https://logowik.com/content/uploads/images/nissan-new2693.logowik.com.webp" />
+              </a>
             </div>
-          <div className='h-40 w-40 flex justify-center items-center'>
-          <a href="https://logowik.com/puma-vector-logo-1-4481.html"><img src="https://logowik.com/content/uploads/images/puma.jpg"/></a>
+            <div className="h-40 w-40 flex justify-center items-center">
+              <a href="https://logowik.com/puma-vector-logo-1-4481.html">
+                <img src="https://logowik.com/content/uploads/images/puma.jpg" />
+              </a>
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-          <a href="https://logowik.com/nikon-vector-logo-2425.html"><img src="https://logowik.com/content/uploads/images/741_nikon.jpg"/></a>
+            <div className="h-40 w-40 flex justify-center items-center">
+              <a href="https://logowik.com/nikon-vector-logo-2425.html">
+                <img src="https://logowik.com/content/uploads/images/741_nikon.jpg" />
+              </a>
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://img.icons8.com/?size=100&id=AbQBhN9v62Ob&format=png&color=000000"/>
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img src="https://img.icons8.com/?size=100&id=AbQBhN9v62Ob&format=png&color=000000" />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://logowik.com/content/uploads/images/mahindra-suv1293.jpg"/>
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img src="https://logowik.com/content/uploads/images/mahindra-suv1293.jpg" />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src={img1}/>
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img src={img1} />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src={img2}/>
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img src={img2} />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src={img3}/>
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img src={img3} />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <a href="https://logowik.com/rapido-bike-taxi-vector-logo-16011.html"><img src="https://logowik.com/content/uploads/images/rapido-bike-taxi8263.jpg"/></a>
+            <div className="h-40 w-40 flex justify-center items-center">
+              <a href="https://logowik.com/rapido-bike-taxi-vector-logo-16011.html">
+                <img src="https://logowik.com/content/uploads/images/rapido-bike-taxi8263.jpg" />
+              </a>
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp9dqdnVXkrnk-DizHUu-a-SuiF3XWqhULbA&s" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp9dqdnVXkrnk-DizHUu-a-SuiF3XWqhULbA&s"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img  src="https://gengigih.demopreview.link/assets/logo-partners/progate-logo.png" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://gengigih.demopreview.link/assets/logo-partners/progate-logo.png"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://logowik.com/content/uploads/images/pizza-hut-new6371.jpg" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://logowik.com/content/uploads/images/pizza-hut-new6371.jpg"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpgHrtdptFtKYOLL4elhWf4pxOfHjNUSxr7Q&s" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSpgHrtdptFtKYOLL4elhWf4pxOfHjNUSxr7Q&s"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://coding-blocks.github.io/assets/images/cb/cblogo.png" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://coding-blocks.github.io/assets/images/cb/cblogo.png"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcikSC2e1l_KFqdwrzZj8AwTpVE06gp7okJQ&s" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcikSC2e1l_KFqdwrzZj8AwTpVE06gp7okJQ&s"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://www.teqfocus.com/wp-content/uploads/2022/08/logo99-1.jpg" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://www.teqfocus.com/wp-content/uploads/2022/08/logo99-1.jpg"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://cdn.dribbble.com/users/873027/screenshots/3996894/dribbble_001_2x.png" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://cdn.dribbble.com/users/873027/screenshots/3996894/dribbble_001_2x.png"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTrEfVcpVCq0lWB0CmMYyM8KBHlxJy1n9RVQ&s" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTrEfVcpVCq0lWB0CmMYyM8KBHlxJy1n9RVQ&s"
+                alt=""
+              />
             </div>
-            <div className='h-40 w-40 flex justify-center items-center'>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZShoVBhCZoflB-I35i_BsVH9Lexeanbbq2Q&s" alt="" />
+            <div className="h-40 w-40 flex justify-center items-center">
+              <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZShoVBhCZoflB-I35i_BsVH9Lexeanbbq2Q&s"
+                alt=""
+              />
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Sponser
+export default Sponser;
