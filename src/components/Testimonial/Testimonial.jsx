5a8f122f71c757574ee93f1d321c240962f61749
Salil Lakra <salillakra.dev@gmail.com>
2025-05-24 16:00:50 +0530

    feat(Testimonial): replace AOS with FadeUpAnimation for improved animation performance

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,28 +1,25 @@
-import React from "react";
+import FadeUpAnimation from "../FadeUp.jsx";
 import Testimonialslider from "./Testimonialslider.jsx";
-import blob from "/blobs.png"
-import blobR from "/blobR.png"
-
 
 function Testimonial() {
-
-    return (
-        <div data-aos="fade-up" data-aos-anchor-placement="center-bottom">
-            <div className="glass-morphic lg:m-20 p-4 lg:p-12 m-6 shadow-2xl rounded-2xl">
-                <div className="flex flex-row justify-start items-center">
-                    <div className="h-[50px] w-[50px] bg-[#FED853]"></div>
-                    <h1 className="text-[25px] ml-[-25px] font-bold">Testimonials</h1>
-                </div>
-                <div className="mt-6">
-                    <h1 className="text-[25px] sm:text-[35px] font-bold ">What Our Mentors Say</h1>
-                </div>
-                <div className="justify-center mt-4 mb-4 md:mx-60 shadow-2xl rounded-lg">
-                    <Testimonialslider />
-                </div>
-            </div>
+  return (
+    <FadeUpAnimation>
+      <div className="glass-morphic lg:m-20 p-4 lg:p-12 m-6 shadow-2xl rounded-2xl">
+        <div className="flex flex-row justify-start items-center">
+          <div className="h-[50px] w-[50px] bg-[#FED853]"></div>
+          <h1 className="text-[25px] ml-[-25px] font-bold">Testimonials</h1>
         </div>
-    )
-
+        <div className="mt-6">
+          <h1 className="text-[25px] sm:text-[35px] font-bold ">
+            What Our Mentors Say
+          </h1>
+        </div>
+        <div className="justify-center mt-4 mb-4 md:mx-60 shadow-2xl rounded-lg">
+          <Testimonialslider />
+        </div>
+      </div>
+    </FadeUpAnimation>
+  );
 }
 
-export default Testimonial
+export default Testimonial;
