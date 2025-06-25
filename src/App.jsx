165b0e64d02716322ecb12c3591bb45b7a2a1f92
Salil Lakra <salillakra.dev@gmail.com>
2025-06-25 23:45:51 +0530

    style: format JSX elements for better readability in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,71 @@
-import React from "react"
-import NavBar from "./components/Navbar.jsx"
-import Footer from "./components/footer/Footer.jsx"
-import Speakerpage from "./otherpages/Speakerpage/Speakerpage.jsx"
-import { createBrowserRouter , RouterProvider } from "react-router-dom"
-import Home from "./Home.jsx"
-import Eventspage from "./otherpages/Eventspage/Eventspage.jsx"
-import Teamspage from "./otherpages/Teamspage/team.jsx"
-import Apppage from "./otherpages/Apppage/Apppage.jsx"
+import NavBar from "./components/layout/Navbar.jsx";
+import Footer from "./components/footer/Footer.jsx";
+import Speakerpage from "./otherpages/Speakerpage/Speakerpage.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./Home.jsx";
+import Eventspage from "./otherpages/Eventspage/Eventspage.jsx";
+import Teamspage from "./otherpages/Teamspage/team.jsx";
+import Apppage from "./otherpages/Apppage/Apppage.jsx";
 
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <><NavBar/><Home/><Footer/></>
+      element: (
+        <>
+          <NavBar />
+          <Home />
+          <Footer />
+        </>
+      ),
     },
     {
       path: "/Speakers",
-      element: <><NavBar/><Speakerpage/><Footer/></>
+      element: (
+        <>
+          <NavBar />
+          <Speakerpage />
+          <Footer />
+        </>
+      ),
     },
     {
       path: "/Events",
-      element: <><NavBar/><Eventspage/><Footer/></>
+      element: (
+        <>
+          <NavBar />
+          <Eventspage />
+          <Footer />
+        </>
+      ),
     },
     {
       path: "/Team",
-      element: <><NavBar/><Teamspage/><Footer/></>
+      element: (
+        <>
+          <NavBar />
+          <Teamspage />
+          <Footer />
+        </>
+      ),
     },
     {
       path: "/App",
-      element: <><NavBar/><Apppage/><Footer/></>
+      element: (
+        <>
+          <NavBar />
+          <Apppage />
+          <Footer />
+        </>
+      ),
     },
+  ]);
 
-  ])
-
-  return(
+  return (
     <div>
-      <RouterProvider router={router}/>
+      <RouterProvider router={router} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
