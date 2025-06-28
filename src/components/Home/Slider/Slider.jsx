1373d854952ddcfae5f33f5384d0a3835d4b60c0
Salil Lakra <salillakra.dev@gmail.com>
2025-06-28 20:02:02 +0530

    feat(Slider): enhance slider functionality with responsive breakpoints and custom navigation buttons

--- src/components/Home/Slider/Slider.jsx
+++ src/components/Home/Slider/Slider.jsx
@@ -1,4 +1,5 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import "./style.css";
 
 import "swiper/css";
 import "swiper/css/effect-coverflow";
@@ -8,6 +9,9 @@ import "swiper/css/navigation";
 import { EffectCoverflow, Pagination, Navigation } from "swiper/modules";
 import cldImageData from "@/data/CldImage.json";
 import CldImage from "@/components/Images/CldImage";
+import { FaArrowRight } from "react-icons/fa";
+import { FaArrowLeft } from "react-icons/fa";
+
 
 function Slider() {
   const slides = cldImageData.Home.slider;
@@ -19,10 +23,19 @@ function Slider() {
           grabCursor={true}
           centeredSlides={true}
           loop={true}
-          slidesPerView={"2"}
+          slidesPerView={1} // Default to 1 slide per view
           breakpoints={{
-            768: {
+            // Large screens
+            1024: {
               slidesPerView: 3.5,
+            },
+            // Medium screens
+            768: {
+              slidesPerView: 2.5,
+            },
+            // Small screens
+            470: {
+              slidesPerView: 1.5,
             },
           }}
           coverflowEffect={{
@@ -31,7 +44,13 @@ function Slider() {
             depth: 100,
             modifier: 2.5,
           }}
-          pagination={{ el: ".swiper-pagination", clickable: true }}
+          pagination={{
+            dynamicBullets: true,
+            dynamicMainBullets: 3,
+            bulletClass: "swiper-pagination-bullet",
+            bulletActiveClass: "swiper-pagination-bullet-active",
+            clickable: true,
+          }}
           navigation={{
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
@@ -41,25 +60,28 @@ function Slider() {
           className="swiper_container"
         >
           {slides.map((img, idx) => (
-            <SwiperSlide key={idx}>
+            <SwiperSlide className="mb-10 relative" key={idx}>
               <CldImage
                 src={img.publicId}
                 alt={img.alt}
                 height={img.height}
                 width={img.width}
-                className="rounded-lg overflow-hidden "
+                className="rounded-lg overflow-hidden w-full"
               />
             </SwiperSlide>
           ))}
 
           <div className="slider-controler">
-            <div className="swiper-button-prev slider-arrow ml-[-1%] animate-bounce">
-              <ion-icon name="arrow-back-outline"></ion-icon>
+            <div className="swiper-button-prev animate-bounce -translate-y-[40%] hover:-translate-y-[40%] slider-arrow">
+              <button className="bg-white/70  p-2 rounded-full shadow-md hover:bg-gray-100 transition-colors">
+                <FaArrowLeft className="w-6 h-6 text-gray-800" />
+              </button>
             </div>
-            <div className="swiper-button-next slider-arrow mr-[-1%] animate-bounce">
-              <ion-icon name="arrow-forward-outline"></ion-icon>
+            <div className="swiper-button-next animate-bounce -translate-y-[40%] hover:-translate-y-[40%] slider-arrow">
+              <button className="bg-white/70 p-2 rounded-full shadow-md hover:bg-gray-100 transition-colors">
+                <FaArrowRight className="w-6 h-6 text-gray-800" />
+              </button>
             </div>
-            <div className="swiper-pagination"></div>
           </div>
         </Swiper>
       </div>
